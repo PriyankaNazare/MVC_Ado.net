e2abc75da243afada29449164aeeb6559f3ce232
DhananjayNazare <dhananjay.nazare@gmail.com>
2022-04-27 00:16:38 -0400

    Added Project

---
+++ MVCWithADO/Controllers/CRUDController.cs
@@ -0,0 +1,71 @@
+﻿using MVCWithADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithADO.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        CRUDModel model = new CRUDModel();
+        public ActionResult Index()
+        {
+            
+            DataTable dt = model.GetAllStudents();
+            return View("Home",dt);
+        }
+
+        public ActionResult Insert()
+        {
+            return View("Create");
+        }
+
+        public ActionResult InsertRecord(FormCollection frm,string action)
+        {
+            if (action == "Submit")
+            {
+                string name = frm["txtName"];
+                int age = Convert.ToInt32(frm["txtAge"]);
+                string gender = frm["gender"];
+                int status = model.InsertStudent(name, age, gender);
+                return RedirectToAction("Index");
+            }
+            else
+                return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int StudentId)
+        {
+            DataTable dt = model.GetStudentById(StudentId);
+            return View("Edit", dt);
+        }
+
+        public ActionResult UpdateRecord(FormCollection frm,string action)
+        {
+            if (action == "Submit")
+            {
+                string name = frm["txtName"];
+                int age = Convert.ToInt32(frm["txtAge"]);
+                string gender = frm["gender"];
+                int id = Convert.ToInt32(frm["hdnId"]);
+                int status = model.UpdateStudent(id, name, age, gender);
+                return RedirectToAction("Index");
+
+            }
+            else
+                return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int StudentId)
+        {
+            int status = model.DeleteStudent(StudentId);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVCWithADO/Models/CRUDModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithADO.Models
+{
+    public class CRUDModel
+    {
+        string con = System.Configuration.ConfigurationManager.AppSettings["connectionstring"];
+
+        public DataTable GetAllStudents()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlcon = new SqlConnection(con))
+            {
+                sqlcon.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Student", sqlcon);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+            }
+            return dt;
+        }
+
+        public DataTable GetStudentById(int studId)
+        {
+            DataTable dt = new DataTable();
+            //string con = System.Configuration.ConfigurationManager.AppSettings["connectionstring"];
+            using(SqlConnection sqlcon=new SqlConnection(con))
+            {
+                sqlcon.Open();
+                SqlCommand cmd = new SqlCommand("Select * from student where student_Id=" + studId,sqlcon);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                sqlcon.Close();
+            }
+            return dt;
+        }
+
+        public int InsertStudent(string StudName,int StudAge,string StudGender)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(con))
+            {
+                sqlcon.Open();
+                string query = "Insert into Student Values(@StudName,@StudAge,@StudGender)";
+                SqlCommand cmd = new SqlCommand(query,sqlcon);
+                cmd.Parameters.AddWithValue("@StudName", StudName);
+                cmd.Parameters.AddWithValue("@StudAge", StudAge);
+                cmd.Parameters.AddWithValue("@StudGender", StudGender);
+                return cmd.ExecuteNonQuery();
+
+            }
+
+        }
+
+        public int UpdateStudent(int StudId, string StudName, int StudAge, string StudGender)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(con))
+            {
+                sqlcon.Open();
+                string query = "Update Student set Student_Name=@StudName,Student_Age=@StudAge, Student_Gender=@StudGender where Student_Id=@StudId";
+                SqlCommand cmd = new SqlCommand(query,sqlcon);
+                cmd.Parameters.AddWithValue("@StudName", StudName);
+                cmd.Parameters.AddWithValue("@StudAge", StudAge);
+                cmd.Parameters.AddWithValue("@StudGender", StudGender);
+                cmd.Parameters.AddWithValue("@StudId", StudId);
+                return cmd.ExecuteNonQuery();
+
+            }
+
+
+        }
+
+        public int DeleteStudent(int StudId)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(con))
+            {
+                sqlcon.Open();
+                string query = "Delete from Student where Student_Id=@StudId";
+                SqlCommand cmd = new SqlCommand(query, sqlcon);
+                cmd.Parameters.AddWithValue("@StudId", StudId);
+                return cmd.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
